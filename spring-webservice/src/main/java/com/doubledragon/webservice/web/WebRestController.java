8970646ae5ea0ccb1071b97aa96eb1acba8d9fa3
dlwnsdyd94 <ljy1237@yonsei.ac.kr>
2021-04-10 18:43:57 +0900

    WebService Program init

---
+++ spring-webservice/src/main/java/com/doubledragon/webservice/web/WebRestController.java
@@ -0,0 +1,13 @@
+package com.doubledragon.webservice.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebRestController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "HelloWorld";
+    }
+}
