2d0546054335ee4e0d079a1cb836c984dfe608b5
dlwnsdyd94 <ljy1237@yonsei.ac.kr>
2021-04-13 13:51:12 +0900

    게시글 등록 - service 메소드 - 입력화면 - 목록 출력 구현

--- spring-webservice/src/main/java/com/doubledragon/webservice/dto/posts/PostsSaveRequestDto.java
+++ spring-webservice/src/main/java/com/doubledragon/webservice/dto/posts/PostsSaveRequestDto.java
@@ -1,18 +1,26 @@
 package com.doubledragon.webservice.dto.posts;
 
 import com.doubledragon.webservice.domain.posts.Posts;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-//@Getter
-//@Setter
+@Getter
+@Setter
 @NoArgsConstructor
 public class PostsSaveRequestDto {
 
     private String title;
     private String content;
     private String author;
+
+    @Builder
+    public PostsSaveRequestDto(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
 
     public Posts toEntity(){
         return Posts.builder()
